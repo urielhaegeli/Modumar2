b7832990baec0e48734db8249a068dfc7eff3f85
Uriel <haegelie@gmail.com>
2026-08-02 15:55:29 -0300

    Corregir base path para despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Modumar2/',
 })
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> 94cd7c5759b7c24f539f649715a9103e7086f451
