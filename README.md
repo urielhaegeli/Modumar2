0d9bbbbc4e749310a49a4975a0af2ee4f80295e2
Uriel <haegelie@gmail.com>
2026-08-02 16:17:58 -0300

    Deploy to gh-pages

--- README.md
+++
@@ -1,1 +0,0 @@
-# Modumar2
--- src/utils/whatsapp.js
+++
@@ -1,6 +0,0 @@
-import { siteConfig } from '../config/siteConfig';
-
-export const openWhatsApp = (message) => {
-  const url = `https://wa.me/${siteConfig.WHATSAPP_NUMBER}?text=${encodeURIComponent(message)}`;
-  window.open(url, '_blank');
-};
